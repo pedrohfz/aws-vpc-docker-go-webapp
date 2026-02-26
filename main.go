aa32a405772b1748b0bf9c38831fddf64138f41b
Pedro Leite <pedrohleite2005@gmail.com>
2026-02-26 20:27:05 -0300

    feat: adição do server starter e status http

--- main.go
+++ main.go
@@ -1,12 +1,17 @@
 package main
 
-import "github.com/gin-gonic/gin"
+import (
+	"net/http"
+
+	"github.com/gin-gonic/gin"
+)
 
 func main() {
 	r := gin.Default()
 	r.GET("/generate", func(c *gin.Context) {
-		c.JSON(200, gin.H{
+		c.JSON(http.StatusOK, gin.H{
 			"password": "example",
 		})
-	}) 
+	})
+	r.Run()
 }
